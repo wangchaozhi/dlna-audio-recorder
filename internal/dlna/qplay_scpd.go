2b0b20ab23b7a3821016be2541cd975deca1f64a
王超智 <50672959+wangchaozhi@users.noreply.github.com>
2026-09-13 16:58:32 +0800

    Add QPlay service description

---
+++ internal/dlna/qplay_scpd.go
@@ -0,0 +1,3 @@
+package dlna
+
+const qplaySCPD = `<?xml version="1.0"?><scpd xmlns="urn:schemas-tencent-com:service-1-0"><specVersion><major>1</major><minor>0</minor></specVersion><actionList><action><name>InsertTracks</name></action><action><name>RemoveTracks</name></action><action><name>GetTracksInfo</name></action><action><name>SetTracksInfo</name></action><action><name>GetTracksCount</name></action><action><name>GetMaxTracks</name></action><action><name>QPlayAuth</name></action></actionList><serviceStateTable></serviceStateTable></scpd>`
