14bfa50be8fbb8c9a4ec11e9e8b728b5d9b971ef
王超智 <50672959+wangchaozhi@users.noreply.github.com>
2026-09-13 16:58:37 +0800

    Add QPlay service fragment

---
+++ internal/dlna/qplay_device_fragment.go
@@ -0,0 +1,3 @@
+package dlna
+
+const qplayServiceXML = `<service><serviceType>urn:schemas-tencent-com:service:QPlay:1</serviceType><serviceId>urn:tencent-com:serviceId:QPlay</serviceId><SCPDURL>/scpd/qplay.xml</SCPDURL><controlURL>/upnp/control/qplay</controlURL><eventSubURL>/upnp/event/qplay</eventSubURL></service>`
